351a0849b33a962eff882196f98a224cb0ba528f
gotharo <gonzalo.castro@gotharo.cl>
2025-09-03 15:52:05 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import NavBar from "./Components/NavBar"
 import Home from "./Components/Home"
 import Footer from "./Components/Footer"
-import RegisterPage from "./Components/auth/RegisterPage"
-import LoginPage from "./Components/auth/LoginPage"
+// import RegisterPage from "./Components/auth/RegisterPage"
+// import LoginPage from "./Components/auth/LoginPage"
 
 function App() {
     return (
         <div className="min-h-screen flex flex-col">
             <NavBar />
             {/* <Home /> */}
-            <RegisterPage />
-            {/* <LoginPage /> */}
+            {/* <RegisterPage /> */}
+            <LoginPage />
             <Footer />
         </div>
     )
